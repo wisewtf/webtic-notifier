206e543414bc82a9501c5eeeb4030acb7015b874
Wise <wise@wise.wtf>
2023-08-21 12:36:36 +0200

    Testing something

--- tools.py
+++ tools.py
@@ -5,7 +5,7 @@ THEATERS_URL = "https://www.webtic.it/proxyWsl/Services/BoWtJsonServices.ashx?da
     
 def configurator(section, value):
     
-    with open("config.toml", "rb") as f:
+    with open("./config.toml", "rb") as f:
         toml_data = tomli.load(f)
 
     configuration = toml_data[section][value]
