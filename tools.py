8a0ddbd93b2f3cd78ea370e6812f59b7efedf8b9
Wise <wise@wise.wtf>
2023-08-21 13:41:42 +0200

    Added argparse to specify config file path

--- tools.py
+++ tools.py
@@ -1,11 +1,17 @@
 import tomli
+import argparse
+
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-c", "--config", help="Config file absolute path")
+args = argParser.parse_args()
 
 CINEMA_ID_PATTERN = r"idcinema=(\d+)"
 THEATERS_URL = "https://www.webtic.it/proxyWsl/Services/BoWtJsonServices.ashx?datasource=CREADW&wtid=webticEventsMc"
     
 def configurator(section, value):
     
-    with open("./config.toml", "rb") as f:
+    with open(f"{args.config}", "rb") as f:
         toml_data = tomli.load(f)
 
     configuration = toml_data[section][value]
