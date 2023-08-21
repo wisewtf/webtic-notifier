3ab19681d98563c275439d936e971522bc50ea8d
Wise <wise@wise.wtf>
2023-08-21 13:52:17 +0200

    Testing argparse default again

--- tools.py
+++ tools.py
@@ -4,12 +4,14 @@ import argparse
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-c", "--config", help="Config file absolute path")
+
+if len(sys.argv)==1:
+    argParser.print_help(sys.stderr)
+    sys.exit(1)
+
 args = argParser.parse_args()
 
-if not vars(args):
-    argParser.print_help()
-else:
-    pass
+
 
 CINEMA_ID_PATTERN = r"idcinema=(\d+)"
 THEATERS_URL = "https://www.webtic.it/proxyWsl/Services/BoWtJsonServices.ashx?datasource=CREADW&wtid=webticEventsMc"
