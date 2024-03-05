5242553b432859c5b05dd9cb63c3f698a99cd7c7
Wise <wise@wise.wtf>
2024-03-05 15:32:28 +0100

    simplifying

--- main.py
+++ main.py
@@ -8,32 +8,12 @@ import theaters
 import webtic
 import anteo
 import urllib.parse
-from datetime import datetime, timedelta
-from pathlib import Path
 
 tools.logger("Started bot")
 
 bot = telebot.TeleBot(tools.configurator('telegram', 'token'))
 
-THEATERS_PICKLE_FILENAME = 'theater_date.pickle'
-THEATERS_PICKLE_PATH = Path(THEATERS_PICKLE_FILENAME)
-
-if not THEATERS_PICKLE_PATH.is_file():
-    tools.logger('Creo cetriolo')
-    tools.pickle_initializer(THEATERS_PICKLE_FILENAME)
-    theaters.theater_updater()
-else:
-    pass
-
-saved_date = tools.unpickler(THEATERS_PICKLE_FILENAME)
-two_weeks_old_date = datetime.now() - timedelta(weeks=2)
-
-if saved_date <= two_weeks_old_date:
-    tools.logger('Aggiorno lista dei cinema')
-    theaters.theater_updater()
-    tools.pickle_initializer(THEATERS_PICKLE_FILENAME)
-else:
-    pass
+theaters.theater_updater()
 
 @bot.message_handler(commands=['tl'])
 def theater_list(message):
