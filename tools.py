4f919da9fa056b1b8f05f25bdae16e7d186882a7
Wise <wise@wise.wtf>
2026-01-28 14:38:29 +0100

    Merging to dev

--- tools.py
+++ tools.py
@@ -2,9 +2,7 @@ import tomli
 import argparse
 import sys
 import requests
-import pickle
-from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 
 argParser = argparse.ArgumentParser(description='The config file path must be specified')  # noqa: E501
 argParser.add_argument("-c", "--config", help="Config file absolute path")
@@ -16,7 +14,6 @@ if len(sys.argv)==1:
 args = argParser.parse_args()
 
 CINEMA_ID_PATTERN = r"idcinema=(\d+)"
-THEATERS_URL = "https://www.webtic.it/proxyWsl/Services/BoWtJsonServices.ashx?datasource=CREADW&wtid=webticEventsMc"
 
 def logger(message):
     print(f"[{datetime.today()}]", message)
@@ -46,33 +43,37 @@ def notifier(body, picture):
         requests.post(apiURL, json={'chat_id': chatID, 'photo': picture, 'caption': message, 'parse_mode': parseMode})  # noqa: E501
     except Exception as e:
         print(e)
-        
-def pickle_initializer(pickle_name: str):
-
-    CHECK_PATH= Path(pickle_name)
-    current_date = datetime.now()
- 
-    if CHECK_PATH.is_file():
-        pass
-    else:
-        with open(pickle_name, "wb") as file:
-            pickle.dump(current_date, file)
             
-def unpickler(pickle_name: str):
-    
-    with open(pickle_name, 'rb') as handle:
-        pickle_data = pickle.load(handle)
-    
-    return pickle_data
-
-def pickler(pickle_name: str, data):
-    
-    with open(pickle_name, 'wb') as handle:
-        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
         
-def command_argument(chat_message):
-    message_parts = chat_message.text.split()
-    return message_parts
+def generate_dates(date):
+    today_date = date
+    date_format = "%Y-%m-%d"
+    start_date = datetime.strptime(today_date, date_format)
+    
+    dates = []
+    while start_date.month == datetime.strptime(today_date, date_format).month:
+        dates.append(start_date.strftime(date_format))
+        start_date += timedelta(days=1)
 
-def remove_duplicates(str: str):
-    return "".join(set(str))
+    return dates
+
+def generate_next_month_date():
+    current_date = datetime.now()
+    first_day_of_next_month = (current_date.replace(day=1) + timedelta(days=32)).replace(day=1)
+    first_day_of_next_month = first_day_of_next_month.date()
+
+    return str(first_day_of_next_month)
+
+def generate_two_month_ahead_date():
+    current_date = datetime.now()
+    first_day_of_next_month = (current_date.replace(day=1) + timedelta(days=64)).replace(day=1)
+    first_day_of_next_month = first_day_of_next_month.date()
+
+    return str(first_day_of_next_month)
+
+def generate_today():
+
+    today_date = datetime.now()
+    formatted_date = today_date.strftime("%Y-%m-%d")
+    
+    return str(formatted_date)
